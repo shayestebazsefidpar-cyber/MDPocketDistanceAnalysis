f222f16b61fa783cb44e95d88f3a44a0ec181916
shayestebazsefidpar-cyber <shayestebazsefidpar@gmail.com>
2026-04-20 15:29:46 +0200

    Formatting

--- src/mdpocketclustering/gbsa_results.py
+++ src/mdpocketclustering/gbsa_results.py
@@ -1,12 +1,16 @@
 import re
+from pathlib import Path
+
+from mdpocketclustering.models import ResidueEnergy
+
 
 def parse_residue_energies(file_path: Path) -> list[ResidueEnergy]:
     """Parse residue energy log files (decompose*.log).
     Returns a list of residues"""
     residues = []
-    pattern = re.compile(r'^\s*([A-Z]{3})-(\d+)\s+([-+]?\d*\.\d+)\s+([-+]?\d*\.\d+)')
+    pattern = re.compile(r"^\s*([A-Z]{3})-(\d+)\s+([-+]?\d*\.\d+)\s+([-+]?\d*\.\d+)")
 
-    with open(file_path, 'r') as f:
+    with open(file_path, "r") as f:
         for line in f:
             match = pattern.match(line)
             if match:
@@ -15,7 +19,7 @@ def parse_residue_energies(file_path: Path) -> list[ResidueEnergy]:
                         residue=match.group(1),
                         index=int(match.group(2)),
                         energy=float(match.group(3)),
-                        std=float(match.group(4))
+                        std=float(match.group(4)),
                     )
                 )
     return residues
