4dd5033445a3835872e78aca9f0c9ed47549a869
shayestebazsefidpar-cyber <shayestebazsefidpar@gmail.com>
2026-05-07 12:59:49 +0200

    to read trajectory has problem

--- src/mdpocketclustering/metrics/rmsd.py
+++ src/mdpocketclustering/metrics/rmsd.py
@@ -8,12 +8,14 @@ class ProteinRMSD:
         u = traj.u
 
         ref = u.select_atoms("protein and backbone")
-        mob = u.select_atoms("protein and backbone")
+        ref_pos = ref.positions.copy()
 
         rmsd = []
 
         for ts in u.trajectory[::stride]:
-            diff = mob.positions - ref.positions
+            mob = u.select_atoms("protein and backbone")
+
+            diff = mob.positions - ref_pos
             rmsd.append(np.sqrt((diff**2).mean()))
 
         return np.array(rmsd)
@@ -22,16 +24,18 @@ class ProteinRMSD:
 class LigandRMSD:
     name = "ligand_rmsd"
 
-    def compute(self, traj, system, stride=1):
+    def compute(self, traj, system, stride=20):
         u = traj.u
 
         ligand = u.select_atoms("resname LIG")
-        ref = ligand.positions.copy()
+        ref_pos = ligand.positions.copy()
 
         rmsd = []
 
         for ts in u.trajectory[::stride]:
-            diff = ligand.positions - ref
+            mob = u.select_atoms("resname LIG")
+
+            diff = mob.positions - ref_pos
             rmsd.append(np.sqrt((diff**2).mean()))
 
         return np.array(rmsd)
